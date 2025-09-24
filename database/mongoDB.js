8af5a5ff9da04f8687a0512e23492fa3d70eeb36
sglavas <sglavas@m.ffzg.hr>
2025-09-24 10:53:42 +0200

    feat: add username field to user model

--- database/mongoDB.js
+++ database/mongoDB.js
@@ -1,4 +1,18 @@
 const { User, Exercise } = require('./models');
 
 
+const createAndSaveUsername = async (user) => {
+    // Create username document
+    const userEntry = new User({ userName: user });
 
+    try{
+        // Save username to the database
+        let result = await userEntry.save()
+
+        return result;
+    }catch(err){
+        console.log(err.message);
+    }
+}
+
+module.exports = { createAndSaveUsername }
