f3b6787192c34f9e842cb43ebab71216becc517c
sglavas <sglavas@m.ffzg.hr>
2025-09-24 07:54:36 +0200

    feat: add database modules and set up connection to MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+
+// Basic configuration
+
+/**
+ * MongoDB connection
+ * @description Connects to the database and logs success or error message
+ */
+const connectToDb = async () =>{
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo DB connection successful.")
+  }catch(error){
+    console.error("MongoDB connection error: ", error.message);
+  }
+}
+
+connectToDb();
+
 
 app.use(cors())
 app.use(express.static('public'))
@@ -11,7 +30,7 @@ app.get('/', (req, res) => {
 
 
 
-
+// First API endpoint
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
