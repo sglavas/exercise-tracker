8aa72673857b104c263f00675552f7d788ac3556
sglavas <sglavas@m.ffzg.hr>
2025-09-28 13:39:58 +0200

    refactor(users): chain POST and GET methods
    - Add missing console log in catc

--- routes/users.js
+++ routes/users.js
@@ -5,36 +5,35 @@ const { createAndSaveUsername, retrieveAllUsers } = require('../database/mongoDB
 // Get data from POST request
 
 
-// Specify /api/users route POST method
-router.post('/', async (req, res) =>{
-  // Get username from input
-  const userNameInput = req.body.username;
+router.route('/')
+    .post(async (req, res) =>{
+        // Get username from input
+        const userNameInput = req.body.username;
 
-  try{
-    // Add username to the database
-    let result = await createAndSaveUsername(userNameInput);
+        try{
+            // Add username to the database
+            let result = await createAndSaveUsername(userNameInput);
 
-    // Get database username and _id
-    const id = result["_id"];
-    const name = result.username;
+            // Get database username and _id
+            const id = result["_id"];
+            const name = result.username;
 
-    // Send response with username and database _id
-    res.json({"username": name, "_id": id});
-  }catch(error){
-    console.log(error.message);
-  }
+            // Send response with username and database _id
+            res.json({"username": name, "_id": id});
+        }catch(error){
+            console.log(error.message);
+        }
+    })
+    .get(async (req, res) =>{
+        try{
+            // Query the User model to get all documents
+            let usersResult = await retrieveAllUsers();
 
-})
-
-// Specify /api/users route GET method
-router.get('/', async (req, res) =>{
-  try{
-    // Query the User model to get all documents
-    let usersResult = await retrieveAllUsers();
-
-    // Send JSON response with all the users
-    res.json(usersResult);
-  }catch(error){}
-})
+            // Send JSON response with all the users
+            res.json(usersResult);
+         }catch(error){
+            console.log(error.message);
+         }
+    });
 
 module.exports = router;
